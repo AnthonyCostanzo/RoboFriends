77d9d4205eeaa9ddf27ca3a89aa4751aa25b1caa
Anthony Costanzo <anthonycostanzo23@gmail.com>
2020-10-12 18:04:45 -0400

    label fixed

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -5,7 +5,7 @@ const SearchBox = (props) =>
     let {searchChange} = props;
     return (
         <div className = 'pa2'>
-        <label for='searchRobots'> SearchRobots </label>
+        <label for='searchRobots' hidden> SearchRobots </label>
         <input id = 'searchRobots' onChange = {searchChange} className='pa3 ba b--green bg-lighest-blue' type = 'search' placeholder = 'search robots'/>
         </div>
     )
