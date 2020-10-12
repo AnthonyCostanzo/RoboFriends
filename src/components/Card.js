1f7cd61b1e191809883871646e141631783cdbb4
Anthony Costanzo <anthonycostanzo23@gmail.com>
2020-10-12 18:10:24 -0400

    pictures optimized

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     const {name,email,id} = props;
     return (
         <div className ='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt = "robot" src ={`https://robohash.org/${id}200*200`} />
+            <img alt = "robot" src ={`https://robohash.org/${id}size=200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
